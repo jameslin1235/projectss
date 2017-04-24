b8e3ad0a2dfcbfdeb2db9462a95fd1446732a0d3
jameslin1235 <jameslin1235@gmail.com>
2017-04-24 10:41:58 -0700

    commit #39

--- project1/posts/views.py
+++ project1/posts/views.py
@@ -18,6 +18,10 @@ from django.http import QueryDict
 def post_list(request):
     if request.method == "GET":
         posts = Post.objects.filter(is_draft = False)
+        posts_count = posts.count
+        no_posts = False
+        if posts_count == 0:
+            no_posts = True
         title = "Latest Posts"
         comment_title = "Comments"
         comment_button_text = "Comment"
@@ -54,14 +58,14 @@ def post_list(request):
             comments_first_pages.append(comments_first_page)
             # check 1
             if post.user == current_user:
-                same_user.append("True")
+                same_user.append(True)
             else:
-                same_user.append("False")
-            # check2
+                same_user.append(False)
+            # check 2
             if post.comment_set.all().count() == 0:
-                no_comments.append("True")
+                no_comments.append(True)
             else:
-                no_comments.append("False")
+                no_comments.append(False)
             comments_count.append(post.comment_set.all().count())
         form = CommentForm()
 
@@ -161,10 +165,12 @@ def post_delete(request,id,slug):
         user = post.user
         current_user = request.user
         if current_user != user:
-            return redirect("posts:post_404")
-        if post.is_draft == True:
+            raise PermissionDenied
+        else:
             post.delete()
             response_data = {}
+            posts_count = user.post_set.all.count()
+            response_data['posts_count']=posts_count
             if post.is_draft == True:
                 response_data['is_draft'] = True
                 return JsonResponse(response_data,safe=False)
@@ -189,28 +195,28 @@ def post_bookmark(request,id,slug):
 def post_like(request,id,slug):
     post = get_object_or_404(Post, id=id)
     if post.is_draft == True:
-        return redirect("posts:post_404")
+        raise PermissionDenied
     if request.GET.get('unlike'):
         post.likes -= 1
     else:
         post.likes += 1
     post.save()
     response_data = {}
-    response_data['like_count'] = post.likes
+    response_data['likes_count'] = post.likes
     return JsonResponse(response_data,safe=False)
 
 @login_required
 def post_dislike(request,id,slug):
     post = get_object_or_404(Post, id=id)
     if post.is_draft == True:
-        return redirect("posts:post_404")
+        raise PermissionDenied
     if request.GET.get('undislike'):
         post.dislikes -= 1
     else:
         post.dislikes += 1
     post.save()
     response_data = {}
-    response_data['dislike_count'] = post.dislikes
+    response_data['dislikes_count'] = post.dislikes
     return JsonResponse(response_data,safe=False)
 
 
@@ -218,7 +224,7 @@ def post_detail(request,id,slug):
     if request.method == "GET":
         post = get_object_or_404(Post, id=id)
         if post.is_draft == True:
-            return redirect("posts:post_404")
+            raise PermissionDenied
         logged_in = False
         if request.user.is_authenticated:
             logged_in = True
@@ -290,7 +296,7 @@ def post_detail(request,id,slug):
         return render(request,"post_detail.html",context)
 
     elif request.method == "POST" and request.is_ajax():
-        post = Post.objects.get(id = id)
+        post = get_object_or_404(Post, id=id)
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
@@ -322,6 +328,7 @@ def post_detail(request,id,slug):
             response_data['number'] = comments_current_page.number
             response_data['page_range'] = comments_current_page.paginator.page_range[-1]
             response_data['success'] = True
+            response_data['message'] = "Comment created."
             return JsonResponse(response_data,safe=False)
         else:
             print('error')
