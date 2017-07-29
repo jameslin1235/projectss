21f382c5f7eafc8465810a9f93049f1d0f4de0ff
jameslin1235 <jameslin1235@gmail.com>
2017-07-29 16:01:29 -0700

    commit #116

--- project1/static/js/post_create.js
+++ project1/static/js/post_create.js
@@ -1,5 +1,5 @@
 tinymce.init({
-  selector: "textarea",
+  selector: "#textarea",
   setup: function(editor) {
     editor.on('input', function(e) {
       tinyMCE.triggerSave();
@@ -11,4 +11,22 @@ tinymce.init({
     "strikethrough underline hr alignjustify forecolor pastetext removeformat charmap outdent indent undo redo"
   ],
   plugins: "lists spellchecker link preview hr textcolor paste charmap autoresize",
+});
+
+
+// $(".js-submit").on("click", function(){
+//
+//   // if (tinyMCE.get("textarea").getContent() == "") {
+//   //   console.log('w');
+//   //   $("#js-required").removeClass("hidden");
+//   //   $(".js-form").submit(function(e){
+//   //     e.preventDefault();
+//   //   });
+//   // }
+//
+// });
+
+$("#textarea").on("invalid", function(e){
+  e.preventDefault();
+  $("#js-required").removeClass("hidden");
 });
